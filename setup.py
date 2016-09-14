78c1da54d3eab3e57feadef3e5006ffdea32fcc5
Jonathan Buys <jonathan.buys@targetsmart.com>
2016-09-14 13:22:37 -0500

    Bumping version number for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cloudchain',
     packages=['cloudchain'],  # this must be the same as the name above
-    version='0.1.3',
+    version='0.1.4',
     description='Secure, easy secrets.',
     author='Jonathan Buys',
     author_email='jonathan.buys@targetsmart.com',
