f615f23825ed596f289c5de18382b768401aefd9
Jonathan Buys <jonathan.buys@targetsmart.com>
2017-12-08 13:36:21 -0600

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cloudchain',
     packages=['cloudchain'],  # this must be the same as the name above
-    version='0.1.4',
+    version='0.1.5',
     description='Secure, easy secrets.',
     author='Jonathan Buys',
     author_email='jonathan.buys@targetsmart.com',
